46c42f237d724662d1b8d5b32b7d613f6c3d3337
nhuzjak20 <nhuzjak20@foi.hr>
2022-10-30 15:59:19 +0100

    commit

--- Python Practice/Guess the Number/main.py
+++ Python Practice/Guess the Number/main.py
@@ -1,1 +1,21 @@
-print("test")
+import random
+
+broj = random.randint(0,30)
+zivoti = 4
+
+ime = input("Unesi ime: ")
+
+while(zivoti >= 0):
+    pogodak = int(input("Pogodi broj izmedu 0 i 30: "))
+    if pogodak > broj:
+        zivoti = zivoti - 1
+        print("Pogodili ste preveliki broj, imate " + str(zivoti) + " pokusaja")
+    elif pogodak < broj:
+        zivoti = zivoti - 1
+        print("Pogodili ste premali broj, imate " + str(zivoti) + " pokusaja")
+    elif pogodak == broj:
+        print("Cestitam, pogodili ste broj i ostalo vam je " + str(zivoti) + " pokusaja")
+        exit()
+    elif zivoti == 0:
+        print("Nemate vise zivota")
+        exit
